059bd4dc5e1701905411c6b90d413b8c6a34f780
JEONG <halyeongj1@gmail.com>
2021-03-10 05:49:15 -0800

    lab3going

--- lab3/src/full_adder.v
+++ lab3/src/full_adder.v
@@ -6,5 +6,7 @@ module full_adder (
     output carry_out
 );
     // TODO: Your code (from lab 2)
-
+    assign sum = a ^ b ^ carry_in;
+    assign carry_out = a&carry_in | a&b | b&carry_in;
+    
 endmodule
--- lab3/src/structural_adder.v
+++ lab3/src/structural_adder.v
@@ -8,5 +8,17 @@ module structural_adder #(
     output [N:0] sum
 );
     // TODO: Your code (from lab 2). You need to parameterize it
-
+    wire [N:0] c;
+    
+    genvar i;
+    
+    generate
+        for (i=0; i<N; i=i+1) begin:bit
+            full_adder add(.a(a[i]), .b(b[i]), .carry_in(c[i]), .sum(sum[i]), .carry_out(c[i+1]));
+        end
+    endgenerate
+    
+    assign c[0] = 1'b0;
+    assign sum[N] = c[N];
+    
 endmodule
