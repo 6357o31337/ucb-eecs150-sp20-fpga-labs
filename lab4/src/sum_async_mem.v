3fa17143ddd498bf075df485fe15b61dfe55859a
JEONG <halyeongj1@gmail.com>
2021-03-13 07:48:56 -0800

    good

--- lab4/src/sum_async_mem.v
+++ lab4/src/sum_async_mem.v
@@ -31,7 +31,8 @@ module sum_async_mem #(
     assign rom_addr = index_reg_val;
     assign index_reg_next = index_reg_val + 1;
     assign index_reg_rst = reset;
-    assign done = (index_reg_val == (size - 1));
+    //assign done = (index_reg_val == (size - 1));
+    assign done = (index_reg_val == size);
     not (index_reg_ce, done);
 
     wire [31:0] sum_reg_val, sum_reg_next;
