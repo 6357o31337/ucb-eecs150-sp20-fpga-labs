b6802949cccdc6020df9cc661802cc9fdb68032d
JEONG <halyeongj1@gmail.com>
2021-03-12 18:35:09 -0800

    edge detector

--- lab3/src/edge_detector.v
+++ lab3/src/edge_detector.v
@@ -11,5 +11,11 @@ module edge_detector #(
       // TODO: implement an edge detector that detects a rising edge of 'signal_in'
       // and outputs a one-cycle pulse at the next clock edge
       // Feel free to use as many number of registers you like
+    genvar i;
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin
+            REGISTER_R reg_r (.q(edge_detect_pulse[i]), .d(signal_in[i]), .rst(edge_detect_pulse[i]), .clk(clk));
+        end
+    endgenerate
 
 endmodule
