1ce1209c98953a28926daf766ed8a5b4f3bd8364
JEONG <halyeongj1@gmail.com>
2021-03-13 16:19:28 -0800

    bad

--- lab4/src/sum_async_mem.v
+++ lab4/src/sum_async_mem.v
@@ -34,7 +34,8 @@ module sum_async_mem #(
     //assign done = (index_reg_val == (size - 1));
     assign done = (index_reg_val == size);
     //not (index_reg_ce, done);
-    assign index_reg_ce = (index_reg_val < size);
+    //assign index_reg_ce = (index_reg_val < size);
+    assign index_reg_ce = (index_reg_val != (size - 1));
 
     wire [31:0] sum_reg_val, sum_reg_next;
     wire sum_reg_rst, sum_reg_ce;
